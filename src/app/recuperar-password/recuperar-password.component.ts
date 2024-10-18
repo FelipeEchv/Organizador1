f9a9563a2aea8ea1753a73a192c13d1553c495e1
Felipe <fe.echeverrias@duocuc.cl>
2024-10-18 16:51:37 -0300

    se arregla recuperar password

--- src/app/recuperar-password/recuperar-password.component.ts
+++ src/app/recuperar-password/recuperar-password.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-recuperar-password',
@@ -10,19 +11,34 @@ export class RecuperarPasswordComponent {
   usuario: string = '';
   email: string = '';
 
-  constructor(private modalController: ModalController) {}
+  constructor(private modalController: ModalController, private http: HttpClient) {}
 
   close() {
     this.modalController.dismiss();
   }
 
   recuperar() {
-    if (this.emailValido()) {
-      alert('Se ha enviado un enlace para recuperar la contraseña a ' + this.email);
-      this.close();
-    } else {
+    if (!this.emailValido()) {
       alert('Por favor, introduzca un correo electrónico válido.');
+      return;
     }
+
+    // Realizar la consulta a la base de datos (db.json)
+    this.http.get<any[]>('http://localhost:3000/usuarios').subscribe(usuarios => {
+      const usuarioEncontrado = usuarios.find(u => u.nombre === this.usuario && u.correo === this.email);
+
+      if (usuarioEncontrado) {
+        // Si se encuentra el usuario y correo
+        alert('Se ha enviado un enlace para recuperar la contraseña a ' + this.email);
+        this.close();
+      } else {
+        // Si no se encuentran los datos
+        alert('No se ha encontrado el usuario o correo proporcionado.');
+      }
+    }, error => {
+      console.error('Error al consultar la base de datos', error);
+      alert('Hubo un error al intentar recuperar la contraseña. Inténtelo más tarde.');
+    });
   }
 
   emailValido(): boolean {
